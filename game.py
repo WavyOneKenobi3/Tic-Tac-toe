54ac5e292281fbfa7e7fd8ace12d483fea30c34b
Mark <markhyman3@gmail.com>
2026-02-28 21:14:27 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,20 +1,29 @@
+print("Player 1 will be X and Player 2 will be O")
 
 #2 players game
+player1 = "Player 1"
+player2 = "Player 2"
+
 
 #board should be printed
-board = []
-for _ in range(9):
-    print(board)
 
-
+def print_board(board):
+    print(f" {board[0]} | {board[1]} | {board[2]}")
+    print("---+---+---")
+    print(f" {board[3]} | {board[4]} | {board[5]}")
+    print("---+---+---")
+    print(f" {board[6]} | {board[7]} | {board[8]}")
+    
+    
+board = ["1","2","3","4","5","6","7","8","9"]
+    
 #accept input of players position 
+def main_game():
+    while True: 
+        print_board(board)
+        position = int(input("Select a position between 1-9"))
 
-
-player1 = input("Choose between X or O: ").strip().upper()
-player2 = None
-if player1 == "X":
-   player2 = "O"
-else:
-    player2 = "X"
     
     
+
+print_board(board)
